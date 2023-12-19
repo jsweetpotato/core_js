7e6369b9049e0930e64d1244266244e6347e463d
jsweetpotato37 <jsweetpotato37@gmail.com>
2023-12-19 11:23:47 +0900

    randomdice

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
-// node server/process를 실행해보자
-// console.log(process);
 
-// node 환경 언어 정보
-console.log(process.env.LANG);
 
-// node로 live-server 실행해보기
+
 const server = require('live-server');
 
 const params = {
   host: 'localhost',
   port: 3000,
   open: false,
-  root:'./client'
+  root: './client'
 }
 
-server.start(params);
+
+server.start(params)
+
+
+
+// console.log(process.env.LANG);
+
+
+
+
+
+
+
+
 
